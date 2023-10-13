0e5a1e0609f7a02352add74d87c285821d4f4ff2
apeng-singlestore <127370261+apeng-singlestore@users.noreply.github.com>
2023-10-13 16:28:02 -0400

    fix chat history and added progress for indexing

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from tabnanny import verbose
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import AIMessage, HumanMessage
-from llama_index import VectorStoreIndex, SimpleDirectoryReader, Document
+from llama_index import VectorStoreIndex
 from llama_index.vector_stores import SingleStoreVectorStore
 from llama_index.llms import ChatMessage, MessageRole
 import gradio as gr
@@ -12,17 +12,17 @@ os.environ["SINGLESTOREDB_URL"] = "admin:password@svc-e6090846-4556-4e7f-b98b-28
 vector_store = SingleStoreVectorStore(table_name="embeddings5", database="demo")
 index = VectorStoreIndex.from_vector_store(vector_store=vector_store)
 
+chat_engine = index.as_chat_engine(
+        chat_mode='context',
+        verbose=True,
+)
+
 def predict(message, history):
     history_llamaindex_format = []
     for human, ai in history:
         history_llamaindex_format.append(ChatMessage(content=human, role=MessageRole.USER))
         history_llamaindex_format.append(ChatMessage(content=ai, role=MessageRole.ASSISTANT))
     history_llamaindex_format.append(ChatMessage(content=message, role=MessageRole.USER))
-    
-    chat_engine = index.as_chat_engine(
-        chat_mode='context',
-        verbose=True,
-    )
 
     return chat_engine.chat(message=message, chat_history=history_llamaindex_format).response
     
--- index.py
+++ index.py
@@ -8,17 +8,18 @@ import openai
 openai.api_key = "sk-xxx"
 
 # load documents
+print("Parsing documents...")
 data_dir = "./data/"
 documents = SimpleDirectoryReader(data_dir).load_data()
-parser = SimpleNodeParser.from_defaults(chunk_size=200, chunk_overlap=20)
+parser = SimpleNodeParser.from_defaults(chunk_size=1000, chunk_overlap=200)
 
 nodes = parser.get_nodes_from_documents(documents)
-print("Document ID:", documents[0].doc_id)
 
+print("Ingesting documents...")
 # Create an index over the documents
 os.environ["SINGLESTOREDB_URL"] = "admin:password@svc-e6090846-4556-4e7f-b98b-28ae9c6aec5f-dml.aws-oregon-3.svc.singlestore.com:3306"
 vector_store = SingleStoreVectorStore(table_name="embeddings5", database="demo")
 storage_context = StorageContext.from_defaults(vector_store=vector_store)
 index = VectorStoreIndex(nodes, storage_context=storage_context)
-print("Index created")
+print("Index created.")
 
