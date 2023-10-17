b4efda4cba0d532cacdb1226680186d91cef4b35
apeng-singlestore <127370261+apeng-singlestore@users.noreply.github.com>
2023-10-16 17:53:50 -0700

    use dotenv for env

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ from llama_index.vector_stores import SingleStoreVectorStore
 from llama_index.llms import ChatMessage, MessageRole
 import gradio as gr
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
-os.environ["OPENAI_API_KEY"] = "sk-xxx"  # Replace with your key
-os.environ["SINGLESTOREDB_URL"] = "admin:password@svc-e6090846-4556-4e7f-b98b-28ae9c6aec5f-dml.aws-oregon-3.svc.singlestore.com:3306"
-vector_store = SingleStoreVectorStore(table_name="embeddings5", database="demo")
+vector_store = SingleStoreVectorStore(table_name="embeddings", database="demo")
 index = VectorStoreIndex.from_vector_store(vector_store=vector_store)
 
 chat_engine = index.as_chat_engine(
-        chat_mode='context',
+        chat_mode='openai',
         verbose=True,
 )
 
@@ -24,7 +24,6 @@ def predict(message, history):
 
     return chat_engine.chat(message=message, chat_history=history_llamaindex_format).response
     
-
 demo = gr.ChatInterface(predict)
 
 if __name__ == "__main__":
--- index.py
+++ index.py
@@ -3,22 +3,20 @@ from llama_index.vector_stores import SingleStoreVectorStore
 from llama_index.storage.storage_context import StorageContext
 from llama_index.node_parser import SimpleNodeParser
 import os
-import openai
-
-openai.api_key = "sk-xxx"
+from dotenv import load_dotenv
+load_dotenv()
 
 # load documents
 print("Parsing documents...")
 data_dir = "./data/"
 documents = SimpleDirectoryReader(data_dir).load_data()
-parser = SimpleNodeParser.from_defaults(chunk_size=1000, chunk_overlap=200)
+parser = SimpleNodeParser.from_defaults(chunk_size=200, chunk_overlap=20)
 
 nodes = parser.get_nodes_from_documents(documents)
 
 print("Ingesting documents...")
 # Create an index over the documents
-os.environ["SINGLESTOREDB_URL"] = "admin:password@svc-e6090846-4556-4e7f-b98b-28ae9c6aec5f-dml.aws-oregon-3.svc.singlestore.com:3306"
-vector_store = SingleStoreVectorStore(table_name="embeddings5", database="demo")
+vector_store = SingleStoreVectorStore(table_name="embeddings", database="demo")
 storage_context = StorageContext.from_defaults(vector_store=vector_store)
 index = VectorStoreIndex(nodes, storage_context=storage_context)
 print("Index created.")
